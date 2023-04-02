e4c5f0c0eed984051d5b6368ee08c15d6c971d02
mfisz <maxifisz@gmail.com>
2023-04-02 20:49:13 -0300

    Vistas

--- src/main.js
+++ src/main.js
@@ -17,6 +17,8 @@ app.use(bodyParser.urlencoded({
     extended: true
 }))
 app.use(cookieParser())
+app.use(express.static("./src/public"))
+
 
 app.set('view engine', 'hbs')
 app.set('views', './src/views')
